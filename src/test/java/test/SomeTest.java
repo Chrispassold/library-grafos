858eb9ab0dd57a3a4a1326c3ca679adb2ecd0795
Christian Passold <passold.christian@hotmail.com>
2018-04-15 15:39:30 -0300

    commit

--- src/test/java/test/SomeTest.java
+++ src/test/java/test/SomeTest.java
@@ -25,31 +25,12 @@
 package test;
 
 import org.junit.Test;
+import static org.junit.Assert.*;
 
-public class SomeTest extends Base {
+public class SomeTest {
 
     @Test public void test1() {
-        run();
-    }
-
-    @Test public void test2() {
-        run();
-    }
-
-    @Test public void test3() {
-        run();
-    }
-
-    @Test public void test4() {
-        run();
-    }
-
-    @Test public void test5() {
-        run();
-    }
-
-    @Test public void test6() {
-        run();
+        assertEquals(1,1);
     }
 
 }
